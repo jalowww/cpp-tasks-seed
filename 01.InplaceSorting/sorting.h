5b50cbf71d9acfc1c34653df55329533e98568cc
jalowww <vladik.gazukin@gmail.com>
2026-06-09 02:32:36 +0300

    Update sorting.h

--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -10,12 +10,63 @@
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+  for (Iterator last = end; last != begin; --last)
+    {
+        bool swapped = false;
+
+        for (Iterator it = begin; it + 1 != last; ++it)
+        {
+            Iterator next = it + 1;
+
+            if (*next < *it)
+            {
+                using std::swap;
+                swap(*it, *next);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+        {
+            return;
+        }
+    }
 }
 
 // --------------------------- QuickSort (например) ---------------------------------------
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+  if (end - begin < 2)
+    {
+        return;
+    }
+    auto pivot = *(begin + (end - begin) / 2);
+    Iterator less_end = begin;
+    Iterator current = begin;
+    Iterator greater_begin = end;
+    while (current != greater_begin)
+    {
+        if (*current < pivot)
+        {
+            using std::swap;
+            swap(*less_end, *current);
+            ++less_end;
+            ++current;
+        }
+        else if (pivot < *current)
+        {
+            --greater_begin;
+            using std::swap;
+            swap(*current, *greater_begin);
+        }
+        else
+        {
+            ++current;
+        }
+    }
+    quick_sort(begin, less_end);
+    quick_sort(greater_begin, end);
 }
 
 #endif // SORTING_H
