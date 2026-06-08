1e2b6f2342984f969cc2d2b1501f2b8892b737a9
jalowww <vladik.gazukin@gmail.com>
2026-06-09 02:35:11 +0300

    Update sort_test.cpp

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,50 @@
+#include <algorithm>
+#include <vector>
 #include <gtest/gtest.h>
 
-// А вот тут collvalue.h не надо, берите просто инты
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
-
-TEST(FooSuite, FooTest)
+namespace
 {
-    EXPECT_EQ(0, 0);
+template <typename Sort>
+void check_sort(Sort sort)
+{
+    std::vector<std::vector<int>> cases = {
+        {},
+        {1},
+        {2, 1},
+        {1, 2, 3, 4, 5},
+        {5, 4, 3, 2, 1},
+        {3, 1, 2, 3, 1, 2},
+        {-5, 10, 0, -1, 7, 7, 3},
+        {42, 42, 42, 42},
+    };
+
+    for (auto data : cases)
+    {
+        auto expected = data;
+        std::sort(expected.begin(), expected.end());
+
+        sort(data.begin(), data.end());
+
+        EXPECT_EQ(data, expected);
+    }
+}
+
+}
+
+TEST(BubbleSort, SortsCorrectly)
+{
+    check_sort([](auto begin, auto end)
+    {
+        bubble_sort(begin, end);
+    });
+}
+
+TEST(QuickSort, SortsCorrectly)
+{
+    check_sort([](auto begin, auto end)
+    {
+        quick_sort(begin, end);
+    });
 }
